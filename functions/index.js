c0877018382a7c75d98fa2708094487cee885b96
Julia Reid <jdereid@gmail.com>
2018-02-18 18:23:41 -0800

    twit var

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// The Firebase Admin SDK to access the Firebase Realtime Database. 
+// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 
 const twit = require('twit');
@@ -11,4 +11,13 @@ admin.initializeApp(functions.config().firebase);
 exports.onPush = functions.https.onRequest((req, res) => {
     console.log(JSON.parse(req.body));
     res.status(200).send(req.body);
+
+    var T = new Twit({
+  consumer_key:         'yYMqR97ivtHAPSXoM0q9E4MNU',
+  consumer_secret:      'xiyuphio0wAwEz6Z4eL5cPbq8YwzERWKaq6t9vgFfJjsOPfdnD',
+  access_token:         '965377684879495168-BqOR9A0IXWLafh9fRXdVMWxtB7vspzH',
+  access_token_secret:  '965377684879495168-BqOR9A0IXWLafh9fRXdVMWxtB7vspzH',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+})
+
 });
